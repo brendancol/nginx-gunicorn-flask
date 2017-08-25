3c8e39aa8697c9f3faab2a5ea4c6088df95559b1
Gustavo <gustavolepri@liberiun.com>
2017-08-25 16:31:37 -0300

    initial flask architecture

---
+++ flask/app.py
@@ -0,0 +1,52 @@
+import os
+import random
+import time
+from flask import Flask, request, render_template, session, flash, redirect, \
+    url_for, jsonify
+from flask.ext.mail import Mail, Message
+from celery import Celery
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top-secret!'
+
+# Flask-Mail configuration
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = 'flask@example.com'
+
+# Celery configuration
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'db+mysql://parietal:parietal@localhost/processing'
+
+
+# Initialize extensions
+mail = Mail(app)
+
+# Initialize Celery
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+
+@celery.task
+def create_job(msg):
+    """ Celery task to create and process job
+    """
+    
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    validate(request.form)
+    job_id = create_job(request.form)
+    results = {}
+    results['job_id'] = job_id
+    response = jsonify(results)
+    response.status_code = 200 or 500 or something
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
