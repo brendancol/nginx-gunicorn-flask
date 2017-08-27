6d6ce671bb7733ac4ab926c9424dcf4cb5a4d511
Gustavo Lepri <gustavolepri@liberiun.com>
2017-08-27 17:38:53 -0300

    install script improved

--- install.sh
+++ install.sh
@@ -8,24 +8,30 @@ rpm -Uvh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0
 # install nginx
 yum install nginx
 
-# install supervisor, gunicorn
-conda install supervisor -y
-conda install gunicorn -y
+# install supervisor, gunicorn, flask
+/home/vagrant/miniconda2/bin/conda install flask -y
+/home/vagrant/miniconda2/bin/conda install flask-cors -y
+/home/vagrant/miniconda2/bin/conda install supervisor -y
+/home/vagrant/miniconda2/bin/conda install gunicorn -y
+/home/vagrant/miniconda2/bin/conda install gevent -y
+
+# using pip
+/home/vagrant/miniconda2/bin/pip install flask-restful
 
 # copy the conf files to the correct place
 
-service nginx stop
-mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf-orig
-cp $APPS_DIR/devops/nginx.conf /etc/nginx/
+sudo service nginx stop
+sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf-orig
+sudo cp $APPS_DIR/devops/nginx.conf /etc/nginx/
 
-service nginx start
+sudo service nginx start
 
 # create supervisor config folder and copy config files to it
-mkdir /etc/supervisor
-cp $APPS_DIR/devops/supervisord.conf /etc/supervisor/
-cp $APPS_DIR/devops/gunicorn_supervisor.ini /etc/supervisor/
+sudo pmkdir /etc/supervisor
+sudo cp $APPS_DIR/devops/supervisord.conf /etc/supervisor/
+sudo cp $APPS_DIR/devops/gunicorn_supervisor.ini /etc/supervisor/
 
-cp $APPS_DIR/devops/supervisord.sh /etc/init.d/
-chmod +x /etc/init.d/supervisor.sh
-/etc/init.d/supervisor.sh start
-chkconfig --add supervisord
+sudo cp $APPS_DIR/devops/supervisord.sh /etc/init.d/
+sudo chmod +x /etc/init.d/supervisor.sh
+sudo /etc/init.d/supervisor.sh start
+sudo chkconfig --add supervisord
