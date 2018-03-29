4d6de32a0f24c4bdd3f808b064051d7bb397be89
bcollins <parietal.io@gmail.com>
2018-03-28 22:33:16 -0500

    added status function to callback

--- kafka_app/app.py
+++ kafka_app/app.py
@@ -1,61 +1,58 @@
 from os import path
 import uuid
 import json
-import datetime
 import cStringIO as StringIO
 from skimage.io import imsave, imread
 
 from flask import Flask, send_file, jsonify, url_for
 
-from kq import Queue, Job
+from kq import Queue
 queue = Queue()
 
 app = Flask(__name__)
 app.debug = True
 
 
-def callback(status, job, result, exception, traceback):
+def update_status(msg, job_id):
+    from os import path
+    import json
+    import datetime
 
-    # update json file status
-    job_id = job.id
-    status_path = '{}.json'.format(str(job_id))
+    here = path.abspath(path.dirname(__file__))
+    status_path = path.join(here, '{}.json'.format(str(job_id)))
 
-    with open('/Users/bcollins/HELLLEOOOE.txt', 'w') as f:
-        f.write(__file__)
-        f.write(status_path)
-    
-    print(status_path)
     if path.exists(status_path):
         with open(status_path) as f:
             content = json.loads(f.read())
     else:
         content = []
 
-    content.append(dict(job=job_id,
-                        status=status,
-                        date=str(datetime.datetime.now())))
-
+    status_msg = dict(job=job_id,
+                      status=msg,
+                      date=str(datetime.datetime.now()))
+    content.insert(0, status_msg)
     with open(status_path, 'w') as f:
         f.write(json.dumps(content))
 
 
-    # print status
+def callback(status, job, result, exception,
+             traceback, update_func=update_status):
+
     if status == 'success':
-        print('The job returned: {}'.format(result))
+        update_func(status, job.key)
 
     elif status == 'timeout':
-        print('The job took too long and timed out')
+        update_func(status, job.key)
 
     elif status == 'failure':
-        print('The job raised an exception on runtime')
-        print(exception)
-
-        # should job be resubmitted to queue?
+        update_func(status + '\nTraceback:\n' + exception, job.key)
 
 
-def run_model(job_id):
+def run_model(job_id, update_func=update_status):
 
     # functions submitted via enqueue need handle their own imports
+    update_func('processing-started', job_id)
+
     import numpy as np
     from skimage.io import imsave
     from time import sleep
@@ -68,36 +65,21 @@ def run_model(job_id):
     file_name = '{}.png'.format(job_id)
 
     # extra sleep so we can see intermediate status values
-    sleep(20)
+    sleep(10)
 
     imsave(file_name, arr,
            plugin='pil', format_str='png')
 
+    update_func('processing-completed', job_id)
     return file_name
 
-
-def update_status(msg, job_id):
-    status_path = '{}.json'.format(str(job_id))
-    print(status_path)
-    if path.exists(status_path):
-        with open(status_path) as f:
-            content = json.loads(f.read())
-    else:
-        content = []
-
-    content.append(dict(job=job_id,
-                        status=msg,
-                        date=str(datetime.datetime.now())))
-
-    with open(status_path, 'w') as f:
-        f.write(json.dumps(content))
 
 @app.route('/')
 def main():
     job_id = str(uuid.uuid4())
-    job = queue.enqueue_with_key(job_id, run_model, job_id)
+    job = queue.enqueue_with_key(job_id, run_model,
+                                 job_id, update_status)
     update_status('submitted', job_id)
-    update_status('submitted2', job_id)
     return jsonify(dict(job_id=job_id,
                         link=url_for('.poll', job=job_id)))
 
@@ -105,7 +87,8 @@ def main():
 @app.route('/<job>')
 def poll(job):
     data_path = '{}.png'.format(job)
-    status_path = '{}.json'.format(job)
+    here = path.abspath(path.dirname(__file__))
+    status_path = path.join(here, '{}.json'.format(str(job)))
     if path.exists(data_path):
         arr = imread(data_path)
         str_buff = StringIO.StringIO()
@@ -117,15 +100,18 @@ def poll(job):
             content = json.loads(f.read())
         return jsonify(dict(status=content))
 
+
 @app.route('/<job>/status')
 def status(job):
-    status_path = '{}.json'.format(job)
+    here = path.abspath(path.dirname(__file__))
+    status_path = path.join(here, '{}.json'.format(str(job)))
     if path.exists(status_path):
         with open(status_path) as f:
             content = json.loads(f.read())
         return jsonify(dict(status=content))
     else:
         return jsonify(dict())
+
 
 if __name__ == '__main__':
     app.run()
